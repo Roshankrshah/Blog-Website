e8cdcba73e8563706ee6cfc2c1375b1d2f352efc
Roshan <shahroshan0720@gmail.com>
2023-08-15 21:47:33 +0530

    login routes setup

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 
 const connectDB = require('./server/config/db');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use(session({
+    secret: 'Blogwebsite making',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
 app.use(express.static('public'));
 
 app.use(expressLayout);
-app.set('layout','./layouts/main');
-app.set('view engine','ejs');
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
-app.use('/',require('./server/routes/main'));
-app.use('/',require('./server/routes/admin'));
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 const port = process.env.PORT || 2023;
-const start = async()=>{
-    try{
+const start = async () => {
+    try {
         await connectDB(process.env.MONGO_URI);
         console.log('Database Connected');
-        app.listen(port,()=>{
+        app.listen(port, () => {
             console.log(`Server is listening on http://localhost:${port}`);
         })
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 };
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+
 const Post = require('../models/Post');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Check Login
+const authMiddleware = (req,res,next)=>{
+    const token = req.cookies.token;
+    if(!token){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    }catch(error){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+}
 
 const adminLayout = '../views/layouts/admin';
 //GET Admin Login Page
@@ -17,5 +37,59 @@ router.get('/admin', async (req, res) => {
         console.log(error);
     }
 });
+
+//POST Check Login
+
+router.post('/admin',async(req,res)=>{
+    try{
+        const {username, password} = req.body;
+        
+        const user = await User.findOne({username});
+
+        if(!user){
+            return res.status(401).json({message:'Invalid Credentials'});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordValid){
+            return res.status(401).json({message:"Invalid Credentials"});
+        }
+
+        const token =  jwt.sign({userId: user._id},process.env.JWT_SECRET);
+        res.cookie('token',token,{httpOnly:true});
+
+        res.redirect('/dashboard');
+    }catch(error){
+        console.log(error);
+    }
+});
+
+// POST user register
+router.post('/register',async(req,res)=>{
+    try{
+        const {username, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        try{
+            const user = await User.create({username,password:hashedPassword});
+            res.status(201).json({message:'User Created',user});
+        }catch(error){
+            
+            if(error.code === 11000){
+                res.status(409).json({message:"User already exist"});
+            }
+            res.status(500).json({message: 'Internal Server Error'});
+        }
+    }catch(error){
+        console.log(error);
+    }
+});
+
+// POST get dashboard
+
+router.get('/dashboard',authMiddleware,async(req,res)=>{
+    res.render('admin/dashboard');
+})
 
 module.exports = router;
